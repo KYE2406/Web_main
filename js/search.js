0a444ed51b641e400db5c9628cda36fa0612fb0a
admin <okyes2406@gmail.com>
2025-04-10 22:42:22 +0900

    6주차 팝업

--- js/search.js
+++ js/search.js
@@ -1,3 +1,27 @@
+//검색창 애니메이션션
+  document.addEventListener("DOMContentLoaded", function () {
+    const searchBtn = document.getElementById("search_btn");
+    const searchInput = document.getElementById("search_input");
+    const wrapper = document.querySelector(".search-wrapper");
+
+    let expanded = false;
+
+    searchBtn.addEventListener("click", function () {
+      if (!expanded) {
+        searchInput.classList.add("show");
+        wrapper.classList.add("expanded");
+        expanded = true;
+        setTimeout(() => searchInput.focus(), 300);
+      } else {
+        searchInput.classList.remove("show");
+        wrapper.classList.remove("expanded");
+        expanded = false;
+      }
+    });
+  });
+
+  
+//검색 시 구글로 이동동
 document.getElementByld("search.btn").addEventListener('clck', search.message);
 
 function search_message() {
